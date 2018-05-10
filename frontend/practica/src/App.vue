bde864e99d0783535d7dbeaff2b882ad48d2fa88
fernando mora romero <fernandomorar88@gmail.com>
2018-05-10 14:24:16 +0200

    Header y Rutas

--- frontend/practica/src/App.vue
+++ frontend/practica/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="container">
+    <div class="container-fluid">
       <pp-header></pp-header>
     </div>
 
@@ -11,6 +11,17 @@
     <div class="container">
       <pp-footer></pp-footer>
     </div>
+    <div class="container">
+      <pp-portada></pp-portada>
+    </div>
+    <div class="container">
+      <pp-nuevas></pp-nuevas>
+    </div>
+    <div class="container">
+      <pp-articulos></pp-articulos>
+    </div>
+
+
   </div>
 </template>
 
@@ -19,24 +30,25 @@
 import Header from './components/Header'
 import Home from './components/Home'
 import Footer from './components/Footer.vue'
+import Portada from './components/portada.vue'
+import Nuevas from './components/Nuevas.vue'
+import Articulos from './components/Articulos.vue'
 
 
 export default {
   components: {
     ppHeader: Header,
     ppHome: Home,
-    ppFooter: Footer
+    ppFooter: Footer,
+    ppPortada: Portada,
+    ppNuevas: Nuevas,
+    ppArticulos: Articulos
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- frontend/practica/src/components/Home.vue
+++ frontend/practica/src/components/Home.vue
@@ -1,7 +1,4 @@
 <template>
-  <div >
-    <h1>hola</h1>
-  </div>
 </template>
 
 <script>
