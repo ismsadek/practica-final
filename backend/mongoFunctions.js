f7f9afbddebf06b64328a757377c0519997b4f9c
unknown <ismsadek@gmail.com>
2018-05-10 11:27:57 +0200

    'mongo'

--- backend/mongoFunctions.js
+++ backend/mongoFunctions.js
@@ -10,24 +10,66 @@ db.once('open', function() {
 console.log("Conectado a MongoDB");
 });
 
-// definicion de esquemas
+// definicion de esquema del artículo
 const articleSchema = mongoose.Schema({
-  text: {
+  author: {
     type: String,
     required: true
-},
-  user: {
+  },
+  title: {
+      type: String,
+      required: true
+  },
+  content: {
     type: String,
     required: true
-},
+  },
   id: {
     type: Number,
     required: true
-}
+  },
+  timeStamp: {
+      type: Number,
+      required: true
+  },
+  
 });
 
-// definicion del modelo de dato
-let userModel = mongoose.model('User', articleSchema);
+// definicion de esquema del articulos nuevos
+const newsSchema = mongoose.Schema({
+  author: {
+    type: String,
+    required: true
+  },
+  title: {
+      type: String,
+      required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: Number,
+    required: true
+  },
+  timeStamp: {
+      type: Number,
+      required: true
+  },
+  url: {
+      type: String,
+      required: true
+  },
+
+  
+});
+
+// definicion del modelo de dato del articulo
+let articleModel = mongoose.model('Article', articleSchema);
+
+// definicion del modelo de dato de nuevos articulos
+let newsModel = mongoose.model('News', newsSchema);
 
 // //insert data
 // function insertData(iText, iUser, iId){
@@ -47,25 +89,26 @@ let userModel = mongoose.model('User', articleSchema);
 
 module.exports = {
    showData: function(){  // Retrieve data
-     userModel.find(function (err, result) {
+     articleModel.find(function (err, result) {
        if (err) return console.error(err);
        if(result){
          console.log(result);
      }
      });
    },
-   findDataById: function (id, text, name, id){ //Modify the data
-     userModel.findById(id, function(err, result) {
+   findDataById: function (id, author, title, content, timeStamp){ //Modify the data
+     articleModel.findById(id, function(err, result) {
        if (err) throw err;
        if(result){
-         result.text = "Paco";
-         result.user = '33';
-         result.id= 2;
+         result.content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum cumque ex, dolor facilis! Velit, non aliquid dolor dolorum iusto sapiente dolore voluptas nulla hic, tempora quam assumenda perferendis ducimus distinctio?";
+         result.author = "Ismael";
+         result.id = 2;
+         result.timeStamp = ;
          result.save();
        }
      });
    }
-
+ 
 }
 
 
